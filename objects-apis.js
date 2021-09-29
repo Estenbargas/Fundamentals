40ad1cac8d9fa1d513ef37da21c584df0a8663f9
Esten Bargas <ebargas1@cnm.edu>
2021-09-29 12:19:58 -0500

    created conditionals-and-functions.js created objects-apis.js created variables-and-data.js

---
+++ objects-apis.js
@@ -0,0 +1,23 @@
+/*let dog = {
+    name: "Steve",
+    color: "brown",
+    breed: "Pug",
+    size: "small",
+    bark: function (typeOfBark) {
+        console.log("Bark!")
+    },
+};
+*/
+
+/*function x(y) {
+    y = y + 5;
+    console.log(y);
+}
+let y = {
+    name: "tom",
+    num: 10,
+}
+let y = 5;
+x(y);
+console.log(y);
+ */
---
+++ variables-and-data.js
@@ -0,0 +1,29 @@
+/*let wordData;
+
+wordData = "Word list";
+
+console.log(wordData)
+
+let subTotal = 100
+let tax = subTotal * 0.07
+let totalWithTax = tax + subTotal;
+*/
+/* let obj = {
+    Key1: "Value 1"
+    Key2: 3,
+    obj2: {
+           obj2Key1: "value of inner object";
+           },
+     };
+     console.log(obj.obj2.obj2Key1);
+ */
+
+/*let add = 1 + 2;
+let sub = 2 - 1;
+let mult = 2*4;
+let division = 4 / 2;
+let mod = 5 % 2;
+
+let string = "string 1 " + "String 2"
+console.log(string);
+*/
